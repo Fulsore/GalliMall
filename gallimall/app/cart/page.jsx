1b01e5a52444b5939df0ee3112bc53a4a872147a
Fulsore <fulsorecoding4268@gmail.com>
2025-07-07 03:14:31 +0530

    Fix AddToCartButton: remove loadCartFromStorage import and usage

--- gallimall/app/cart/page.jsx
+++ gallimall/app/cart/page.jsx
@@ -7,7 +7,6 @@ import { verifyPayment } from '../Redux/Slice/orderSlice';
 import Link from 'next/link';
 import axios from 'axios';
 import Image from 'next/image';
-// import { loadCartFromStorage } from '../Redux/Slice/cartSlice';
 
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8000';
